f3642f7491dd2c424ec8a801d601c60cea5bf89e
Mher Kazandjian <mherkazandjian@gmail.com>
2012-05-31 20:25:45 +0200

    - wrote the template to produce grids of plots

--- meshUtils.py
+++ meshUtils.py
@@ -404,7 +404,7 @@ class meshArxv():
         radexObj.setInFile( inFile )
 
         transitionNum = self.radexParms['plotTransitionInGrid']
-        every = 1
+        every = 100
         nDone = 0
         upper = None
         lower = None
