cb0026e0b98825fd9f9c0ee8df008e0e6ea7674d
Mher Kazandjian <mherkazandjian@gmail.com>
2014-09-21 23:40:46 +0200

    isloated the tests and included data needed to run the pdr test case

--- tests/run_singleMesh.py
+++ tests/run_singleMesh.py
@@ -14,6 +14,8 @@ from amuse.community.pdr import interface
 HOME = os.environ['HOME']
 
 nWorker = 1  # number of proccesses
+
+dataDir     = HOME + '/ism/code/ismcpak/data/' 
 outputDir   = HOME + '/ism/runs/oneSided/single_mesh/'
 
 pdr = interface.pdrInterface( number_of_workers = nWorker, redirection='none') 
@@ -24,13 +26,13 @@ lMech      = 1e-50
 
 #---------------------amuse parameters------------------------------------------------
 pdr.set_outputDir                  (outputDir + 'meshes/');
-pdr.set_species_fName              (HOME + '/ism/speciesInfo/species.inp');
-pdr.set_underUbundant_fName        (HOME + '/ism/speciesInfo/underabundant.inp');
-pdr.set_rate99_fName               (HOME + '/ism/speciesInfo/rate99.inp');
-pdr.set_selfSheilding_CO_fName     (HOME + '/ism/speciesInfo/self_shielding_CO.inp');
-pdr.set_rotationalCooling_baseName (HOME + '/ism/speciesInfo/rotationalcooling/rotcool');
-pdr.set_vibrationalCooling_baseName(HOME + '/ism/speciesInfo/vibrationalcooling/vibcool');
-pdr.set_database_fName             (HOME + '/ism/database/z-1.0.dat');
+pdr.set_species_fName              (dataDir + 'species.inp');
+pdr.set_underUbundant_fName        (dataDir + 'underabundant.inp');
+pdr.set_rate99_fName               (dataDir + 'rate99.inp');
+pdr.set_selfSheilding_CO_fName     (dataDir + 'self_shielding_CO.inp');
+pdr.set_rotationalCooling_baseName (dataDir + 'rotationalcooling/rotcool');
+pdr.set_vibrationalCooling_baseName(dataDir + 'vibrationalcooling/vibcool');
+pdr.set_database_fName             (dataDir + 'z-1.0.dat');
 pdr.set_zeta                       (5.0e-17);
 pdr.set_S_depletion                (200.0);
 pdr.set_TTol                       (1e-3);
