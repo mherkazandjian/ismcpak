7109d0389bb6fb7bc7d95be7807cd70da138d1c6
Mher Kazandjian <mherkazandjian@gmail.com>
2019-02-13 06:16:26 +0100

    add the singularity recipe and cleanup the single mesh example

--- tests/run_singleMesh.py
+++ tests/run_singleMesh.py
@@ -11,38 +11,38 @@ import os
 
 from amuse.community.pdr import interface
 
-HOME = os.environ['HOME']
+root = '/'
 
 nWorker = 1  # number of proccesses
 
-dataDir     = HOME + '/ism/code/ismcpak/data/' 
-outputDir   = HOME + '/ism/runs/tests/oneSided/single_mesh/'
+dataDir     = os.path.join(root, 'ism/ismcpak/data/')
+outputDir   = os.path.join(root, 'ism/runs/tests/oneSided/single_mesh')
 
-pdr = interface.pdrInterface( number_of_workers = nWorker, redirection='none') 
+pdr = interface.pdrInterface(number_of_workers = nWorker, redirection='none')
 
 log10_nGas = 1.0
 log10_G0   = 3.0
 lMech      = 1e-50
 
 #---------------------amuse parameters------------------------------------------------
-pdr.set_outputDir                  (outputDir + 'meshes/');
-pdr.set_species_fName              (dataDir + 'pdr/species.inp');
-pdr.set_underUbundant_fName        (dataDir + 'pdr/underabundant.inp');
-pdr.set_rate99_fName               (dataDir + 'pdr/rate99.inp');
-pdr.set_selfSheilding_CO_fName     (dataDir + 'pdr/self_shielding_CO.inp');
-pdr.set_rotationalCooling_baseName (dataDir + 'pdr/rotationalcooling/rotcool');
-pdr.set_vibrationalCooling_baseName(dataDir + 'pdr/vibrationalcooling/vibcool');
-pdr.set_database_fName             (dataDir + 'pdr/z-1.0.dat');
-pdr.set_zeta                       (5.0e-17);
-pdr.set_S_depletion                (200.0);
-pdr.set_TTol                       (1e-3);
-pdr.set_CTol                       (1e-3);
-pdr.set_metalicity                 (1.0);
-pdr.set_AvMax                      (30.0);
-pdr.set_slabSizeCrit               (0.5);
-pdr.set_min_deltaAv                (0.01);
-pdr.set_max_deltaAv                (0.5);
-pdr.set_maxSlabs                   (200);
+pdr.set_outputDir                  (outputDir + 'meshes/')
+pdr.set_species_fName              (dataDir + 'pdr/species.inp')
+pdr.set_underUbundant_fName        (dataDir + 'pdr/underabundant.inp')
+pdr.set_rate99_fName               (dataDir + 'pdr/rate99.inp')
+pdr.set_selfSheilding_CO_fName     (dataDir + 'pdr/self_shielding_CO.inp')
+pdr.set_rotationalCooling_baseName (dataDir + 'pdr/rotationalcooling/rotcool')
+pdr.set_vibrationalCooling_baseName(dataDir + 'pdr/vibrationalcooling/vibcool')
+pdr.set_database_fName             (dataDir + 'pdr/z-1.0.dat')
+pdr.set_zeta                       (5.0e-17)
+pdr.set_S_depletion                (200.0)
+pdr.set_TTol                       (1e-3)
+pdr.set_CTol                       (1e-3)
+pdr.set_metalicity                 (1.0)
+pdr.set_AvMax                      (30.0)
+pdr.set_slabSizeCrit               (0.5)
+pdr.set_min_deltaAv                (0.01)
+pdr.set_max_deltaAv                (0.5)
+pdr.set_maxSlabs                   (200)
 #------------------------------------------------------------------------------------
 
 # initializing variables and running the models
