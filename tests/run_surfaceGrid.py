86a2de22c01dd209f83e72db8e6d533c974bd629
Mher Kazandjian <mherkazandjian@gmail.com>
2021-07-01 03:04:24 +0200

    modfity / improve the surface grid script

--- tests/run_surfaceGrid.py
+++ tests/run_surfaceGrid.py
@@ -1,4 +1,30 @@
-#---------------------------------------------------------------------------------------------------
+"""
+<keywords>
+example, pdr, amuse, static, surface, heating, grid
+</keywords>
+<description>
+run a grid of PDR models to compute the surface heating. The purpose of
+this grid is to be used as input for other grids where the mechanical
+heating will be added as a fraction of the surface heating that is
+computed in this grid.
+
+The following is done in this script:
+
+   - run the grid
+   - pack the models into an models archive (database)
+
+run using the command:
+
+    $ cd $ISMCPAK/test
+    $ mpirun -np 1 run_surfaceGrid.py
+
+</description>
+"""
+#--------------------------------------------------------------------------------------------------
+import os
+import sys
+import time
+import pickle
 import matplotlib
 matplotlib.use('PS')
 from numpy import *
@@ -10,7 +36,7 @@ from mesh import *
 from chemicalNetwork import *
 from enumSpecies import *
 from ismUtils import *
-import time, sys, os
+from meshUtils import meshArxv
 
 nWorker = 1  # number of proccesses
 pdr     = interface.pdrInterface( channel_type = 'mpi', number_of_workers = nWorker, redirection='none')
@@ -29,7 +55,7 @@ if not os.path.isdir(meshes_dir):
 pdr.set_outputDir                  (outputDir + 'meshes/');
 pdr.set_species_fName              (dataDir + "pdr/species.inp");
 pdr.set_underUbundant_fName        (dataDir + "pdr/underabundant.inp");
-pdr.set_rate99_fName               (dataDir + "pdr/rate99.inp");
+pdr.set_rate99_fName               (dataDir + "pdr/rate99Fixed.inp");
 pdr.set_selfSheilding_CO_fName     (dataDir + "pdr/self_shielding_CO.inp");
 pdr.set_rotationalCooling_baseName (dataDir + "pdr/rotationalcooling/rotcool");
 pdr.set_vibrationalCooling_baseName(dataDir + "pdr/vibrationalcooling/vibcool");
@@ -45,6 +71,27 @@ pdr.set_min_deltaAv                (0.01);
 pdr.set_max_deltaAv                (0.5);
 pdr.set_maxSlabs                   (100);
 
+# ======== dump parameter to be used in the analysis to a pickle file =========
+# .. todo:: due to some formatting issues/inconvience the files used by ismcpak
+# are slightly different from the ones used the C PDF code, hence the slightly
+# different paths in the dict below
+parms = {
+    'dirPath'      : outputDir,
+    'relativeGmech': False,
+    'metallicity'  : metallicity,
+    'chemistry'    : {
+                      'rxnFile'       : dataDir + "rate99Fixed.inp",
+                      'specNumFile'   : dataDir + "species.inp",
+                      'underAbunFile' : dataDir + "underabundant.inp",  # .. todo:: fix this in chemicalNetwork.py / ignore first line
+                      'removeManual'  : ['13CH3'],
+                      'baseSpecies'   : 'baseSpeciesDefault', #name of the module holding the base species
+                      'umistVer'      : 'umist99',
+                     }
+}
+pickle.dump(parms, open(os.path.join(outputDir, 'used_parms.pkl'), 'w'))
+# ========================= done dumping parameter ============================
+
+# define the grid parameters of the models
 dx   = 0.5      # log10 density
 xMin = 0.0
 xMax = 6.0
@@ -107,5 +154,12 @@ f = file(outputDir+'results.out', 'w')
 for i in arange(n):
     f.write( '%d %.3f %.3f %.3e %.d\n' % (ids[i], rho[i], G0[i], Lmech[i], mshErr[i]))
 f.close()
+
+# constructing the archive
+print 'pack the meshes into a database'
+t0 = time.time()
+arxvW = meshArxv(dirPath=outputDir)
+arxvW.construct(meshNamePrefix='mesh', writeDb=True)
+print 'time constructing %f' % (time.time() - t0)
 
 print 'done'
